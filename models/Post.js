fe02427459bf00d3e17615affe80f37acb2fc9d5
Emre Samurlu <emrsmrl@gmail.com>
2021-10-10 00:55:42 +0300

    CleanBlog Project v1.2

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    detail: String,
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Model
+const Post = mongoose.model('Post', PostSchema);
+
+// Exports
+module.exports = Post;
