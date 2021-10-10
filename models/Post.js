509f7563137358125fdfd0dbeffc30502cf6ccca
Emre Samurlu <emrsmrl@gmail.com>
2021-10-10 14:16:27 +0300

    CleanBlog Project v1.3

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title: String,
     detail: String,
+    name: String,
     dateCreated: {
         type: Date,
         default: Date.now,
