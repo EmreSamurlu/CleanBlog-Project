f72748556047d9c470c83c7372a41bc8086c5725
Emre Samurlu <emrsmrl@gmail.com>
2021-10-10 22:28:05 +0300

    CleanBlog Project v1.4

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Third-party modules
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
+// Core Modules
 const ejs = require('ejs');
-const path = require('path');
 
-const Post = require('./models/Post');
+// My Modules
+const pageController = require('./controllers/pageControllers');
+const postController = require('./controllers/postController');
 
 const app = express();
 
@@ -21,32 +25,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
 
 // Routes
-app.get('/', async (req, res) => {
-    const posts = await Post.find({});
-    res.render('index', {
-        posts,
-    });
-});
-app.get('/posts/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render('post',{
-        post,
-    })
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-app.get('/add_post', (req, res) => {
-    res.render('add_post');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
-app.post('/posts', async (req, res) => {
-    await Post.create(req.body);
-    res.redirect('/');
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost); // Add post
+app.put('/posts/:id', postController.updatePost); // Update post
+app.delete('/posts/:id', postController.deletePost); // Delete post
 
+//
 const port = 3000;
 
 app.listen(port, () => {
